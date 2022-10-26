88cde7c5499bb1d99f39f8a720b74d547f2b1819
Ehsan <az202719@gmail.com>
2022-10-26 03:27:14 +0200

    Improving the app's styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 import { Task } from './task/task';
-
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -38,5 +39,23 @@ export class AppComponent {
       event.previousIndex,
       event.currentIndex
     );
+  }
+  constructor(private dialog: MatDialog) {}
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {},
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult|undefined) => {
+        if (!result) {
+          return;
+        }
+        this.todo.push(result.task);
+      });
   }
 }
